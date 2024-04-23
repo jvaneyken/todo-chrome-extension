d9f4cd6e8e65546ba6b320a7b6d38683ce361c0a
Josh van Eyken <josh.vaneyken@gmail.com>
2024-04-22 18:03:29 -0700

    Create TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Typography } from "@mui/material";
 
-import AppHeader from './components/AppHeader.tsx';
-
+import AppHeader from "./components/AppHeader.tsx";
+import TodoList from "./components/TodoList.tsx";
 
 function App() {
-  return <>
-  <Typography variant="h2">Hello World</Typography>
-  <AppHeader />
-  </>;
+  return (
+    <>
+      <Typography variant="h2">Hello World</Typography>
+      <AppHeader />
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,9 @@
+const TodoList = () =>  {
+    return(
+        <>
+            <div>I am the TodoList component</div>
+        </>
+    );
+}
+
+export default TodoList;
